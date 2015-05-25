c84a2a0a82fd7bfd3f81e5b2d27a3ccb67e75813
immber <jessiekrushing@hotmail.com>
2015-05-25 01:21:41 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  resources :customers
+  resources :customers do
+    collection do
+      get :authenticate
+      get :oauth_callback
+    end
+  end
+
+  root to: 'customers#index'
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
